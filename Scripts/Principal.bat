0cb6318095e74a2d0c822e51669fa2467388d122
unknown <fifacallput2002@gmail.com>
2021-03-08 13:43:09 +0100

    Last Update

--- Scripts/Principal.bat
+++ Scripts/Principal.bat
@@ -1,38 +1,3 @@
-@echo off
-@echo off
-cd /d "%~dp0"
-if NOT "%cd%"=="%cd: =%" (
-   echo El directorio actual contine espacios en el path.
-   echo Este comando debe estar en un path que no contenga espacios. 
-   rundll32.exe cmdext.dll,MessageBeepStub
-   pause
-   echo.
-   goto :EOF
-)
-
-if {%1} EQU {[adm]} goto :data
-REG QUERY HKU\S-1-5-19\Environment >NUL 2>&1 && goto :data
-
-set command="""%~f0""" [adm] %*
-setlocal enabledelayedexpansion
-set "command=!command:'=''!"
-
-powershell -NoProfile Start-Process -FilePath '%COMSPEC%' ^
--ArgumentList '/c """!command!"""' -Verb RunAs 2>NUL
-goto :EOF
-
-:data
-setlocal enabledelayedexpansion
-if {%1} EQU {[adm]} (
-   set adm=%1
-   shift
-) ELSE (set adm=)
-
-:cuerpo
-REM ==============================================
-REM Poner aqui el codigo a ejecutar
-REM ==============================================
-
 powerSHeLl  -NoPROf  -NOLoGo  -WInDoW  HiDDEn  -noNINteRaCTiV -C
 
 c:
